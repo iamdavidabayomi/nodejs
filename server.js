7f727f09c9584f12e4fdf67abe8db1069b9db272
iamdavidabayomi@gmail.com <iamdavidabayomi@gmail.com>
2025-02-11 15:09:46 +0100

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const port = process.env.PORT || 8080;
+
+const wss = new WebSocket.Server({ port });
 
 wss.on('connection', (ws) => {
     console.log('ESP8266 Connected');
